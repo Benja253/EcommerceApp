51b6206cbc6557d430873f3f574859915c46ad90
Benja253 <benja48.9@gmail.com>
2022-04-09 13:18:04 -0400

    Start Project

---
+++ src/ECommerceApp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ECommerceApp = () => {
+  return (
+    <div>ECommerceApp</div>
+  )
+}
+
+export default ECommerceApp
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import ECommerceApp from './ECommerceApp'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ECommerceApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
