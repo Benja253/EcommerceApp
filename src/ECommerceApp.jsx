64032f46eae9e8bb6cdb04ba35593b4ee679577d
Benjamín Flores <benja@MacBook-Pro-de-Benjamin.local>
2022-04-13 11:23:08 -0400

    Empezando la ruta /product/:id

--- src/ECommerceApp.jsx
+++ src/ECommerceApp.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { HashRouter, Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
+import Product from './components/product/Product'
 
 const ECommerceApp = () => {
   return (
     <HashRouter>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/product/:id' element={<Product />} />
       </Routes>
     </HashRouter>
   )
---
+++ src/components/product/Product.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HeaderHome from '../HeaderHome'
+
+const Product = () => {
+  return (
+    <div className='product-container'>
+      <HeaderHome />
+      
+    </div>
+  )
+}
+
+export default Product
